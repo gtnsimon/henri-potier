06b908307ddf124f9c59b3dfc42e19a783f6d1cf
Gaëtan Simon <gaetan.simon3@gmail.com>
2021-10-29 01:04:34 +0200

    feat: footer to indicate who did this

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -7,7 +7,13 @@
     />
 
     <!-- Page content -->
-    <nuxt />
+    <nuxt class="layout-page" />
+
+    <footer class="gtnsimon">
+      <b-container>
+        <p class="gtnsimon__by">Proposé par <a href="https://gtnsimon.dev/?gh=henri-potier" target="_blank" rel="noopener noreferrer" title="Travailler avec Gaëtan SIMON">Gaëtan SIMON</a>, <em>développeur web passionné</em> - <a href="https://github.com/gtnsimon/henri-potier" target="_blank" title="Consulter le code source du projet">GitHub</a></p>
+      </b-container>
+    </footer>
 
     <!-- Cart feedback on item added on top of content -->
     <cart-feedback
